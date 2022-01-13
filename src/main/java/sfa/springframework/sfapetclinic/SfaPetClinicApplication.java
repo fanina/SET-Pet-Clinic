ea855ec29b588f23f12544ee3fe6d6a3787718e8
safae Fanina <safae.fanina@gmail.com>
2022-01-13 15:09:38 +0100

    Initial project ADD

---
+++ src/main/java/sfa/springframework/sfapetclinic/SfaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package sfa.springframework.sfapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfaPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sfa/springframework/sfapetclinic/SfaPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package sfa.springframework.sfapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SfaPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
